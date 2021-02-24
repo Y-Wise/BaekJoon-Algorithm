af5dfc5087991f1b278d735a6239ce4a6dc122e7
Y-Wise <Y-Wise@users.noreply.github.com>
2021-02-24 19:00:02 +0900

    별찍기, X보다작은수
    for문 finish

--- BJstepAlgo/Step03.cpp
+++ BJstepAlgo/Step03.cpp
@@ -45,7 +45,7 @@ void NPrint() {
 }
 
 //3-7단계 11021
-void AplusBminus7() {
+void AplusB_7() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 
@@ -61,7 +61,7 @@ void AplusBminus7() {
 }
 
 //3-8단계 11022
-void AplusBminus8() {
+void AplusB_8() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 
@@ -74,4 +74,52 @@ void AplusBminus8() {
 		cout << "Case #" << i << ": " << a << " + " << b << " = " << sum << "\n";
 
 	}
+}
+
+//3-9단계 2438
+void Star_1() {
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= i; j++) {
+			cout << "*";
+		}
+		cout << "\n";
+	}
+}
+
+//3-10단계 2439
+void Star_2() {
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		for (int j = n; j > i; j--) {
+			cout << " ";
+		}
+
+		for (int j = 1; j <= i; j++) {
+			cout << "*";
+		}
+
+		cout << "\n";
+	}
+}
+
+//3-11단계 10871_1
+void SmallbyX() {
+	int n, x;
+	cin >> n >> x;
+	int* seqA = new int[n];
+	for (int i = 0; i < n; i++) {
+		cin >> seqA[i];
+	}
+	for (int i = 0; i < n; i++) {
+		if (seqA[i] < x) {
+			cout << seqA[i] << " ";
+		}
+	}
+}
+
+//3-11단계 10871_2
+void SmallbyX_2() {
 }
--- BJstepAlgo/function.h
+++ BJstepAlgo/function.h
@@ -1,4 +1,13 @@
 //Step03 for¹®
+#include<iostream>
+using namespace std;
+
+//3rd for
 void FastAplusB();
 void PrintN();
 void NPrint();
+void AplusB_7();
+void AplusB_8();
+void Star_1();
+void Star_2();
+void SmallbyX();
