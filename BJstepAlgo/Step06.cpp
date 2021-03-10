33c6cb570c935711d8c75aa185cba9af4a69725a
Y-Wise <Y-Wise@users.noreply.github.com>
2021-03-10 20:52:26 +0900

    셀프 넘버

--- BJstepAlgo/Step06.cpp
+++ BJstepAlgo/Step06.cpp
@@ -2,11 +2,7 @@
 #include<vector>
 using namespace std;
 
-long long sum(vector<int>& a);
-
 int main() {
-	vector<int> a = {};
-	cout << sum(a) << endl;
 
 }
 
@@ -21,5 +17,34 @@ long long sum(vector<int> &a) {
 }
 
 //6-2단계 4673
+int d(int n) {
+	// n <= 10000
+	int tmpN = n;
+	int sum = n;
+
+	//n + n의 자리수 합
+	for (int i = 0; i < 5; i++) {
+		sum += tmpN % 10;
+		tmpN /= 10;
+	}
+
+	return sum;
+}
+
+int arrDN[10001] = { 0 };
+void Constructor() {
+	int n = 1;
+	while (true) {
+		arrDN[d(n)]++;
+		n++;
+		if (n > 10000) break;
+	}
+
+	for (int i = 1; i < 10001; i++) {
+		if (arrDN[i] == 0) {
+			cout << i << endl;
+		}
+	}
+}
 
 //6-3단계 1065
