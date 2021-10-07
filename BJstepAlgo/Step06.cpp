4ffb49abce3062ba0762fa644493f68fba82eff8
dkrkek96@soongsil.ac.kr <66158433+Y-Wise@users.noreply.github.com>
2021-10-08 00:40:10 +0900

    step6 - 3rd - no.1065

--- BJstepAlgo/Step06.cpp
+++ BJstepAlgo/Step06.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<vector>
-#include<string>
+#include<cmath>
+
 using namespace std;
 
 //================함수=======================
@@ -39,7 +40,7 @@ void Constructor() {
 
 	for (int i = 1; i < 10001; i++) {
 		if (arrDN2[i] == 0) {
-			cout << i << endl;
+			std::cout << i << std::endl;
 		}
 	}
 }
@@ -48,59 +49,54 @@ void Constructor() {
 void Hansu() {
 	// n 이하의 수들 중 자리수들이 등차수열인 수(한수)의 개수 출력
 	// 1 <= n <= 1000
-
-	int n; // 임의의 n
-	int cntHansu; // 한수의 개수
 	//한자리수 : 전부 한수(1~9)
 	//두자리수 : 전부 한수(1~9)
 	//*세자리수 : 이 부분만 구하면 됨*
 	//네자리수 : 1000 (한수 아님)
-	int arrN[] = {0, 0, 0, 0};
-	for(int i = 0; i<4; i++){
-	}
+
+	int n; // 임의의 n
+	int cntHansu{0}; // 한수의 전체 개수
+	int arrI[] = {0, 0, 0, 0}; // 자릿수에 맞춰 배열에 넣기
 
 	// 임의의 N 입력 받기
 	std::cin >> n;
-	string strN = to_string(n);
-	strN[0];
-	if (n < 10) {
-		cntHansu = n;
-	}
-	else if (n < 100) {
-		cntHansu = n;
-	}
-	else if (n < 1000) {
 
+	//arrN[0] = (n % 10000) / 1000;
+	//arrN[1] = (n % 1000) / 100;
+	//arrN[2] = (n % 100) / 10;
+	//arrN[3] = n % 10;
+
+	
+	if (n < 100) {
+		cntHansu = n;
 	}
-	else if(n == 1000){
-		
+	else{
+		cntHansu = 99;
+		for (int i = 100; i <= n; i++) {
+			// arrI{1000의 자리, 100의자리, 10의자리, 1의자리}
+			for (int j = 0; j < 4; j++) {
+				int r = pow(10, 4 - j);
+				int d = pow(10, 3 - j);
+				arrI[j] = (i % r) / d;
+			}
+			
+			if (i < 1000) {
+				// 한수일 때만 한수의 개수 증가
+				if (arrI[1] - arrI[2] == arrI[2] - arrI[3]) {
+					cntHansu++;
+				}
+			}
+			else {
+				if (arrI[0] - arrI[1] == arrI[1] - arrI[2] &&
+					arrI[1] - arrI[2] == arrI[2] - arrI[3]) {
+					cntHansu++;
+				}
+			}
+		}
 	}
-	else {}
 
 	// 한수의 개수 출력
-	std::cout << cntHansu;
-
-	//int n, tmpK;
-	//int arrK[4]{ 0 };
-	//cin >> n;
-	//int cnt{ 0 };
-	//int cntK{ 0 };
-
-	//int k{ 1 };
-	//while (k <= n) {
-	//	tmpK = k;
-	//	for (int i = 3; i >= 0; i--) {
-	//		arrK[i] = tmpK % 10;
-	//		tmpK /= 10;
-	//	}
-	//	for (int i = 0; i < 4; i++) {
-	//		if (arrK[i + 1] == arrK[i]) {
-	//			cnt++;
-	//		}
-	//	}
-	//	k++;
-	//}
-	//cout << cnt;
+	std::cout << cntHansu << std::endl;
 }
 
 int main() {
