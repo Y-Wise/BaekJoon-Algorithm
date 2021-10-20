bbab03f1045b68a90577754e8a86061e07cbac46
dkrkek96@soongsil.ac.kr <66158433+Y-Wise@users.noreply.github.com>
2021-10-21 00:03:52 +0900

    revise 8-1

--- BJstepAlgo/Step08.cpp
+++ BJstepAlgo/Step08.cpp
@@ -3,38 +3,30 @@
 // 8-1단계 1712번 손익분기점
 void BreakEvenPoint()
 {
-	int a{}; // 고정 비용 A
-	int b{}; // 가변 비용 B
-	int c{};// 노트북 C만원
-
+	// 21억 이하 자연수
+	// 고정 비용 A, 가변 비용 B, 노트북 C만원
+	int a{}, b{}, c{}; 
+	
 	std::cin >> a >> b >> c;
+	int num; // 노트북 개수 = a / (c - b);
 
-	int revenue{}; // 수익 revenue = c * num;
-	int expense{}; // 비용 expense = a + (b * num);
-
-	// 총수입 > 총비용이 최초로 발생하는 지점 == 손익분기점
-	int num; // 노트북 개수 num = a / (c - b);
-	/*
-	revenue > expense
-	(c-b) * num > a
-	num > a/(c-b)
+	/* 손익분기점
+	총수익 > 총비용이 최초로 발생하는 지점
+	수익 = c * num;
+	비용 = a + (b * num);
+	(c-b) * num > a;
+	num > a/(c-b);
 	*/
-	if (c - b != 0) {
-		num = a / (c - b);
-		if (num < 0) {
-			num = -1;
-		}
-		else {
-			num++;
-		}
+
+	if (c - b <= 0) {
+		num = -1;
 	}
 	else {
-		num = -1;
+		num = a / (c - b) +1;
 	}
 
 	std::cout << num;
 }
 
 int main() {
-	BreakEvenPoint();
 }
