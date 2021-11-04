48de79a81d13aa0a8b802d4d7b9c1c67e19389b3
dkrkek96@soongsil.ac.kr <66158433+Y-Wise@users.noreply.github.com>
2021-11-05 00:04:24 +0900

    step10-1 revise

--- BJstepAlgo/Step10.cpp
+++ BJstepAlgo/Step10.cpp
@@ -1,20 +1,28 @@
 #include<iostream>
-// 10단계 10872 팩토리얼
+// 10-1단계 10872 팩토리얼
 // n*(n-1)*(n-2)*...2*1
-int FacN(int n) {
+int Factorial(int n) {
 	if (n == 0) {
 		return 1;
 	}
 	else {
-		return n * FacN(n - 1);
+		return n * Factorial(n - 1);
 	}
 }
-void Factorial() {
+void FacN() {
 	int N; // 0이상의 정수
 	std::cin >> N;
-	std::cout << FacN(N);
+	std::cout << Factorial(N);
 }
 
+
+// 10-2단계 10870 피보나치 수 5
+void Fibonacci5() {
+
+}
+
+
+
+
 int main() {
-	Factorial();
 }
