597da591f1d692017e06ffd541cf963d6c66098d
dkrkek96@soongsil.ac.kr <66158433+Y-Wise@users.noreply.github.com>
2021-11-06 23:41:04 +0900

    step 10-2. no.10870 Success

--- BJstepAlgo/Step10.cpp
+++ BJstepAlgo/Step10.cpp
@@ -9,20 +9,24 @@ int Factorial(int n) {
 		return n * Factorial(n - 1);
 	}
 }
-void FacN() {
-	int N; // 0이상의 정수
-	std::cin >> N;
-	std::cout << Factorial(N);
-}
-
 
 // 10-2단계 10870 피보나치 수 5
-void Fibonacci5() {
-
+int Fibonacci5(int n) {
+	int a, b;
+	if (n == 0) return 0;
+	else if(n == 1) return 1;
+	else {
+		return Fibonacci5(n - 1) + Fibonacci5(n - 2);
+	}
 }
-
-
-
 
 int main() {
+	int N; // 0이상의 정수
+	std::cin >> N;
+	std::cout << Fibonacci5(N);
 }
+
+//f5
+//f4+f3
+//f3+      f2 +     f2+  f1
+//f2+f1 + f1+f0  +  1+0 + 1
