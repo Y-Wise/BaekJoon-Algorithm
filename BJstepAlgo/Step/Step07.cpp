7924b3fc7825373e144ae8c7c99d2add8ab8ab12
dkrkek96@soongsil.ac.kr <66158433+Y-Wise@users.noreply.github.com>
2022-01-10 23:29:19 +0900

    Step07 no.1157 wordStudy()

--- BJstepAlgo/Step/Step07.cpp
+++ BJstepAlgo/Step/Step07.cpp
@@ -1,13 +1,17 @@
 #include<iostream>
 #include<string>
 #include<cstring>
+#include<vector>
+#include<algorithm>
 
+//================문자열=======================
 //7-1단계 11654
 void ASCII() {
 	char c{};
 	std::cin >> c;
 	std::cout << (int)c;
 }
+
 
 //7-2단계 11720
 void SumN() {
@@ -24,6 +28,7 @@ void SumN() {
 
 	std::cout << sum;
 }
+
 
 //7-3단계 10809
 void Alphabet() {
@@ -62,7 +67,6 @@ void Alphabet() {
 	}
 }
 
-
 void Alphabet2() {
 	char S[101] = { 0, }; // 입력받을 단어 S
 	char alphabet[26] = { 0, };
@@ -97,3 +101,78 @@ void Alphabet2() {
 		std::cout << alphabetPos[i] << " ";
 	}
 }
+
+
+//7-4단계 2675 문자열 반복
+
+
+
+//7-5단계 1157 단어공부
+void WordStudy() {
+	std::string word{}; //입력받을 단어
+	std::cin >> word;
+	
+	int maxCnt{}; //최대 알파벳 개수
+	int maxPos{}; //최대 알파벳이 뭔지 찾는 위치
+	int cntAlphabet[26] = { 0, }; //사용된 알파벳
+	int asc{}; // 아스키코드값 : 대문자(65~90), 소문자(97~122)
+	
+	// 개수 세기
+	for (int i = 0; i < word.length(); i++) {
+		asc = (int)word[i];
+		
+		if (asc >= 65 && asc <= 90) {
+			//대문자
+			asc -= 65;
+			cntAlphabet[asc]++;
+		}
+		else if(asc >= 97 && asc <= 122){
+			//소문자
+			asc -= 97;
+			cntAlphabet[asc]++;
+		}
+		else {
+			//알파벳이 아닌 경우
+			std::cout << "not Alphabet\n";
+		}
+	}
+
+	// 최다 알파벳 찾기
+	for (int i = 0; i < 26; i++) {
+		if (cntAlphabet[i] > maxCnt) {
+			maxCnt = cntAlphabet[i];
+			maxPos = i;
+		}
+	}
+
+	// 최다 알파벳이 2개 이상인지 찾기
+	bool duplicationAlphabet = false;
+	for (int i = 0; i < 26; i++) {
+		if (i != maxPos) {
+			if (cntAlphabet[i] == maxCnt) {
+				duplicationAlphabet = true;
+			}
+		}
+	}
+
+	// 결과
+	if (duplicationAlphabet == true) {
+		std::cout << "?";
+	}
+	else {
+		std::cout << (char)(maxPos + 65);
+	}
+}
+
+
+//7-5단계 1157 단어공부
+void WordStudy2() {
+	//std::sort(cntAlphabet[0], cntAlphabet[25]); //정렬
+
+}
+
+
+int main() {
+	WordStudy();
+	return 0;
+} 
