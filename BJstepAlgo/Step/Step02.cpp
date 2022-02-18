79bb1c92347e4b2776d829162b7d31df64b554ea
dkrkek96@soongsil.ac.kr <66158433+Y-Wise@users.noreply.github.com>
2022-02-19 01:59:35 +0900

    Completed 2-7th no.2480

--- BJstepAlgo/Step/Step02.cpp
+++ BJstepAlgo/Step/Step02.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 //2-6단계 2525
 void OvenClock() {
 	int si{}, boon{}; // 23이하, 59이하
@@ -15,6 +16,40 @@ void OvenClock() {
 	std::cout << resultSi << " " << resultBoon;
 }
 
+//2-7단계 2480
+void ThreeDice() {
+	int reward{};
+	int dicesArr[3] = {};
+	
+	for (int i = 0; i < sizeof(dicesArr)/sizeof(int); i++) {
+		std::cin >> dicesArr[i];
+	}
+
+	if (dicesArr[0] == dicesArr[1] && dicesArr[1] == dicesArr[2]) {
+		reward = 10000 + dicesArr[0] * 1000;
+	}
+	else if (dicesArr[0] == dicesArr[1] || dicesArr[0] == dicesArr[2] || dicesArr[1] == dicesArr[2]) {
+		int same{};
+		if (dicesArr[0] == dicesArr[1] || dicesArr[0] == dicesArr[2]) same = dicesArr[0];
+		else same = dicesArr[1];
+		reward = 1000 + same * 100;
+	}
+	else {
+		int maxD = dicesArr[0];
+		//maxD = std::max(dicesArr[0], std::max(dicesArr[1], dicesArr[2]));
+
+		for(int i = 1; i < sizeof(dicesArr) / sizeof(int); i++) {
+			maxD = std::max(maxD, dicesArr[i]);
+		}
+
+		reward = maxD * 100;
+	}
+
+	std::cout << reward << "\n";
+}
+
 int main() {
-	OvenClock();
+	while (1) {
+		ThreeDice();
+	}
 }
