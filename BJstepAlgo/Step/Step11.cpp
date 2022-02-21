55a9897b358971f05d0804484e41ab89190ccf60
dkrkek96@soongsil.ac.kr <66158433+Y-Wise@users.noreply.github.com>
2022-02-22 00:18:54 +0900

    Completed 11-1st no.2798

--- BJstepAlgo/Step/Step11.cpp
+++ BJstepAlgo/Step/Step11.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<vector>
-#include<algorithm>
+
+//11-1단계 2798
 void BlackJack() {
 	int N{}, M{}; // 카드 개수, 딜러가 말하는 숫자(3장의 합보다 큼)
 	std::vector<int> cardNums;
@@ -12,42 +13,19 @@ void BlackJack() {
 		cardNums.push_back(num);
 	}
 
-	std::sort(cardNums.begin(), cardNums.end(), std::greater<int>());
-
-	for (int i = 0; i < N; i++) {
-		std::cout << cardNums[i] << " ";
-	}
-
-	int sum{};
+	int sum{}, max{};
 	bool failM = false;
-	for (int i = 0; i < N; i++) {
-		sum += cardNums[i];
-		if (sum > M) sum -= cardNums[i]; continue;
-
-		for (int j = i+1; j < N; j++) {
-			sum += cardNums[j];
-			if (sum > M) sum -= cardNums[j]; continue;
-
-			for (int k = j+1; k < N; k++) {
-				sum += cardNums[k];
-				if (sum <= M) {
-					failM = false;
-					break;
-				}
-				else {
-					sum -= cardNums[k];
-					failM = true;
-				}
+	for (int i = 0; i < N-2; i++) {
+		for (int j = i+1; j < N-1; j++) {
+			for (int k = j + 1; k < N; k++) {
+				sum = cardNums[i] + cardNums[j] + cardNums[k];
+				if (sum > max && sum <= M) max = sum;
 			}
-			if (failM == true) sum -= cardNums[j];
-			else break;
 		}
-		if (failM == true) sum -= cardNums[i];
-		else break;
 	}
-	std::cout << sum << "\n";
+	std::cout << max << "\n";
 }
 
+
 int main() {
-	BlackJack();
 }
